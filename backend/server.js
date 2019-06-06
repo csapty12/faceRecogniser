c7a0f38b3585d9bcc649dd67b37e162ef7a58ca9
Carl Saptarshi <carlsaptarshi@rentalcars.com>
2019-06-06 10:08:49 +0100

    [node] connected to docker db instance and all routes working

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
+const knex = require("knex");
+const bcrypt = require("bcrypt");
 
+const database = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "password",
+    database: "postgres",
+    // schema: "face_detector",
+    port: "54322"
+  }
+});
+
+database
+  .select("*")
+  .from("login")
+  .then(data => {
+    console.log(data);
+  });
 const app = express();
 
 let db = {
@@ -30,38 +50,68 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  console.log(req.body);
-  if (
-    db.users[0].email === req.body.email &&
-    db.users[0].password === req.body.password
-  ) {
-    return res.json("success");
-  }
-  return res.status(404).json("user not found");
+  database
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return database
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to fetch user"));
+      }
+      return res.status(400).json("username and password mismatch");
+    })
+    .catch(err => res.status(400).json("username and password mismatch"));
 });
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
+  const hash = bcrypt.hashSync(password, 0);
 
-  db.users.push({
-    id: "3",
-    name: name,
-    email: email,
-    password: password,
-    joined: new Date()
-  });
-  return res.json(db.users[db.users.length - 1]);
+  database
+    .transaction(trx => {
+      trx
+        .insert({
+          hash: hash,
+          email: email
+        })
+        .into("login")
+        .returning("email")
+        .then(loginEmail => {
+          return trx("users")
+            .returning("*")
+            .insert({ name: name, email: loginEmail[0], joined: new Date() })
+            .then(user => {
+              return res.json(user[0]);
+            });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch(e => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  db.users.forEach(user => {
-    if (user.id === id) {
-      return res.json(user);
-    }
-  });
-  return res.status(404).json("no user found");
+  database
+    .select("*")
+    .from("users")
+    .where("id", id)
+    .then(user => {
+      if (user.length) {
+        return res.json(user[0]);
+      }
+      return res.status(400).json("no user found");
+    });
 });
+
 app.listen(3002, () => {
   console.log("app deployed on port 3002");
 });
